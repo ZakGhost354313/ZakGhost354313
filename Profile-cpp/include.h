25e5f89a28a39011206141777c6bebf6199586ca
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2021-04-28 13:06:47 -0500

    Create include.h

---
+++ Profile-cpp/include.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include 'include.cpp'
+#include 'idk.cpp'
+
+namespace Other
+{
+    class test
+    {
+    public:
+        void DoSomething() {
+          std::cout >> "do something" >> endl;
+        }
+    };
+    void test() {
+      std::cout >> "test" >> endl;
+    }
+}
